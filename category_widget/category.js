83a4e735c55645d51f8691e05e72fd8b9e04b4c6
ktugan <kadir.tugan@gmail.com>
2014-11-28 16:45:06 +0100

    improved category widget

--- category_widget/category.js
+++ category_widget/category.js
@@ -1,22 +1,3 @@
-function reduceAdd(p, v) {
-    v.groups.forEach (function(val, idx) {
-        p[val] = (p[val] || 0) + 1; //increment counts
-    });
-    return p;
-}
-
-function reduceRemove(p, v) {
-    v.groups.forEach (function(val, idx) {
-        p[val] = (p[val] || 0) - 1; //decrement counts
-    });
-    return p;
-
-}
-
-function reduceInitial() {
-    return {};
-}
-
 ExampleApp.directive('category', ['MetadataService', function(MetadataService) {
 
     return {
@@ -29,12 +10,30 @@ ExampleApp.directive('category', ['MetadataService', function(MetadataService) {
             scope.$on('init', function() {
                 var data = MetadataService.getData();
                 scope.dimCategory = data.dimension(function(d){return d.groups;});
-                scope.groupCategory = scope.dimCategory.groupAll().reduce(reduceAdd, reduceRemove, reduceInitial);
+                scope.groupCategory = scope.dimCategory.groupAll().reduce(scope.reduceAdd, scope.reduceRemove, scope.reduceInitial);
 
             });
 
             scope.$on('filterChanged', function() {
             });
+
+            scope.reduceAdd = function (p, v) {
+                v.groups.forEach (function(val, idx) {
+                    p[val] = (p[val] || 0) + 1; //increment counts
+                });
+                return p;
+            };
+
+            scope.reduceRemove = function(p, v) {
+                v.groups.forEach (function(val, idx) {
+                    p[val] = (p[val] || 0) - 1; //decrement counts
+                });
+                return p;
+            };
+
+            scope.reduceInitial = function() {
+                return {};
+            }
 
         }
     };
