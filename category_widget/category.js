e408ef0c9f2b453f499f840cadf41ca690f7354c
ktugan <kadir.tugan@gmail.com>
2014-11-28 16:45:06 +0100

    merge

--- category_widget/category.js
+++ category_widget/category.js
@@ -5,17 +5,48 @@ ExampleApp.directive('category', ['MetadataService', function(MetadataService) {
         templateUrl: 'category_widget/category.html',
         scope: {},
         link: function(scope, element, attrs) {
-            scope.categories = [];
+            scope.categories = {};
 
             scope.$on('init', function() {
                 var data = MetadataService.getData();
                 scope.dimCategory = data.dimension(function(d){return d.groups;});
-                scope.groupCategory = scope.dimCategory.groupAll().reduce(scope.reduceAdd, scope.reduceRemove, scope.reduceInitial);
+                //temporary, second needed so the group always has the newest values, don't know why
+                var tmpCategory = data.dimension(function(d){return d.groups;});
+                scope.groupCategory = tmpCategory.groupAll().reduce(scope.reduceAdd, scope.reduceRemove, scope.reduceInitial);
+                for(var key in scope.groupCategory.value())
+                {
+                    scope.categories[key] = {}
+                    scope.categories[key].checked = true;
+                    scope.categories[key].size = scope.groupCategory.value()[key];
+                }
 
             });
 
             scope.$on('filterChanged', function() {
+                for(var key in scope.groupCategory.value())
+                    scope.categories[key].size = scope.groupCategory.value()[key];
             });
+
+            scope.categoryChecked = function(index)
+            {
+                var checkedCategories = [];
+                for(var key in scope.categories)
+                    if(scope.categories[key].checked)
+                        checkedCategories.push(key);
+
+                var filterFunction = function(d)
+                {
+                    var tmp = d.filter(function(n) {
+                        return checkedCategories.indexOf(n) != -1
+                    });
+                    return tmp.length > 0;
+                }
+
+                scope.dimCategory.filter(filterFunction);
+                MetadataService.triggerUpdate();
+                scope.$apply();
+            }
+
 
             scope.reduceAdd = function (p, v) {
                 v.groups.forEach (function(val, idx) {
