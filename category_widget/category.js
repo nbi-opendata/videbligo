29aa6b46974fad742270541fa401bb79497de95b
ktugan <kadir.tugan@gmail.com>
2014-11-28 13:02:51 +0100

    category widget

---
+++ category_widget/category.js
@@ -0,0 +1,53 @@
+function reduceAdd(p, v) {
+    v.groups.forEach (function(val, idx) {
+        p[val] = (p[val] || 0) + 1; //increment counts
+    });
+    return p;
+}
+
+function reduceRemove(p, v) {
+    v.groups.forEach (function(val, idx) {
+        p[val] = (p[val] || 0) - 1; //decrement counts
+    });
+    return p;
+
+}
+
+function reduceInitial() {
+    return {};
+}
+
+ExampleApp.directive('category', ['MetadataService', function(MetadataService) {
+
+    return {
+        restrict: 'AE',
+        templateUrl: 'category_widget/category.html',
+        scope: {},
+        link: function(scope, element, attrs) {
+            scope.categories = [];
+
+            scope.$on('init', function() {
+                var data = MetadataService.getData();
+                scope.dimCategory = data.dimension(function(d){return d.groups;});
+                scope.groupCategory = scope.dimCategory.groupAll().reduce(reduceAdd, reduceRemove, reduceInitial);
+
+            });
+
+            scope.$on('filterChanged', function() {
+                /*
+                scope.categories = [];
+                scope.groupCategory.all().forEach(function(cat)
+                {
+                    for(var i = 0; i < cat.key.length; i++)
+                    {
+                        if(scope.categories[cat.key[i]] == undefined)
+                            scope.categories[cat.key[i]] = 0;
+                        scope.categories[cat.key[i]] += cat.value;
+                    }
+
+                });*/
+            });//end broadcast on
+
+        }
+    };
+}]);
