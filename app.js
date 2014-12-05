e02ec9cb586e34f5e9ffeecee4eb80bae65b22e3
ktugan <kadir.tugan@gmail.com>
2014-12-05 13:34:01 +0100

    reformat

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 var Videbligo = angular.module('Videbligo', []);
 
-Videbligo.service('MetadataService',["$rootScope", "$location", "$http", function($rootScope, $location, $http){
+Videbligo.service('MetadataService', ["$rootScope", "$location", "$http", function ($rootScope, $location, $http) {
     var crossData = null;
     var all = null;
-    this.init = function() {
+    this.init = function () {
         $http({
             method: 'GET',
             url: "./assets/dist/data.json"
-        }).success(function(data) {
+        }).success(function (data) {
             crossData = crossfilter(data);
             all = crossData.groupAll();
             $rootScope.$broadcast('init');
             $rootScope.$broadcast('filterChanged');
-        }).error(function(data) {
+        }).error(function (data) {
             console.log("Request failed");
         });
     };
     this.init();
 
-    this.getData = function(){
+    this.getData = function () {
         return crossData;
     }
 
-    this.triggerUpdate = function() {
+    this.triggerUpdate = function () {
         $rootScope.$broadcast('filterChanged');
     }
 
-    this.length = function()
-    {
+    this.length = function () {
         return all.value();
     }
 
 }]);
 
 function parseDate(input) {
-    if(input == undefined)
+    if (input == undefined)
         return undefined;
     var parts = input.split('-');
     return new Date(parts[0], parts[1], parts[2]); // Note: months are 0-based
