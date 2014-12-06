e61e156b8df5f1a4d1a3a914833eac5b4f204d97
lbenedix <lukas.benedix@fu-berlin.de>
2014-12-07 00:04:46 +0100

    added a set datastructure

--- videbligo.js
+++ videbligo.js
@@ -49,3 +49,61 @@ function parseDate(input) {
     var parts = input.split('-');
     return new Date(parts[0], parts[1], parts[2]); // Note: months are 0-based
 }
+
+function StringSet() {
+    var setObj = {}, val = {};
+
+    this.add = function(str) {
+        setObj[str] = val;
+    };
+
+    this.contains = function(str) {
+        return setObj[str] === val;
+    };
+
+    this.remove = function(str) {
+        delete setObj[str];
+    };
+
+    this.values = function() {
+        var values = [];
+        for (var i in setObj) {
+            if (setObj[i] === val) {
+                values.push(i);
+            }
+        }
+        return values;
+    };
+}
+
+var category_mapping = {
+    'arbeit':        'Arbeitsmarkt',
+    'bildung':       'Bildung',
+    'demographie':   'Demographie',
+    'geo':           'Geographie und Stadtplanung',
+    'gesundheit':    'Gesundheit',
+    'jugend':        'Jugend',
+    'kultur':        'Kunst und Kultur',
+    'sicherheit':    'Öffentliche Sicherheit',
+    'verwaltung':    'Öffentliche Verwaltung, Haushalt und Steuern',
+    'protokolle':    'Protokolle und Beschlüsse',
+    'erholung':      'Sport und Erholung',
+    'tourismus':     'Tourismus',
+    'verentsorgung': 'Ver- und Entsorgung',
+    'verkehr':       'Verkehr',
+    'wahl':          'Wahlen',
+    'wirtschaft':    'Wirtschaft',
+    'wohnen':        'Wohnen und Immobilien',
+    'sonstiges':     'Sonstiges',
+    'sozial':        'Sozialleistungen',
+
+    /* mapping is not one-to-one (just one dataset, maybe a bug) */
+    /* http://daten.berlin.de/datensaetze/wlan-standorte-berlin */
+    'oeffentlich':   'Sonstiges'
+
+    /* categories, that are not in use and therefor have no mapping right now
+    '': 'Transport und Verkehr',
+    '': 'Umwelt und Klima',
+    '': 'Verbraucherschutz'
+    */
+};
