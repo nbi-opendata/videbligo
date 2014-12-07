362a65a0d8fc28457de9e04fe5931d7ad8b98643
Martin Sonnenholzer <martin.sonnenholzer@googlemail.com>
2014-12-07 22:52:59 +0100

    add mapping for granularity

--- geographicalgranularity_widget/mapping.js
+++ geographicalgranularity_widget/mapping.js
@@ -1,3 +1,37 @@
-/**
- * Created by martin on 07.12.14.
- */
+var geographicalGranularity = {
+    Block           : {
+        name : 'Block',
+        size : 0,
+        elements : 0
+    },
+    Hausnummer      : {
+        name : 'Hausnummer',
+        size : 0,
+        elements : 0
+    },
+    Bezirk          : {
+        name : 'Bezirk',
+        size : 0,
+        elements : 0
+    },
+    Bezirksregion   : {
+        name : 'Bezirksregion',
+        size : 0,
+        elements : 0
+    },
+    Berlin          : {
+        name : 'Berlin',
+        size : 0,
+        elements : 0
+    },
+    Planungsraum    : {
+        name : 'Planungsraum',
+        size : 0,
+        elements : 0
+    },
+    Others          : {
+        name : 'Andere',
+        size : 0,
+        elements : 0
+    }
+}
