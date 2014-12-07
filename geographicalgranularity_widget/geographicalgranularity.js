50afcb1c1077cf1c06f8d0988867bbaed7fb7ea7
Martin Sonnenholzer <martin.sonnenholzer@googlemail.com>
2014-12-07 17:18:47 +0100

    expanded the sceleton

--- geographicalgranularity_widget/geographicalgranularity.js
+++ geographicalgranularity_widget/geographicalgranularity.js
@@ -5,7 +5,18 @@ Videbligo.directive('search', ['MetadataService', function(MetadataService) {
         templateUrl: 'geographicalgranularity_widget/geographicalgranularity.html',
         scope: {},
         link: function(scope, element, attrs) {
+            scope.data = null;
+            scope.geographicalDimension = null;
 
+            scope.init = function(){
+                scope.data = MetadataService.getData();
+                scope.geographicalDimension = scope.data.dimension(function(d){return d;});
+            }
+
+            scope.$on('filterChanged', function() {
+            });
+
+            MetadataService.registerWidget(scope.init);
         }
     };
 }]);
--- timegranularity_widget/timegranularity.js
+++ timegranularity_widget/timegranularity.js
@@ -5,7 +5,18 @@ Videbligo.directive('search', ['MetadataService', function(MetadataService) {
         templateUrl: 'timegranularity_widget/timegranularity.html',
         scope: {},
         link: function(scope, element, attrs) {
+            scope.data = null;
+            scope.geographicalDimension = null;
 
+            scope.init = function(){
+                scope.data = MetadataService.getData();
+                scope.geographicalDimension = scope.data.dimension(function(d){return d;});
+            }
+
+            scope.$on('filterChanged', function() {
+            });
+
+            MetadataService.registerWidget(scope.init);
         }
     };
 }]);
