8392aef9f38692e4742a1b29dc5552f10b032af1
Martin Sonnenholzer <martin.sonnenholzer@googlemail.com>
2014-12-07 22:53:29 +0100

    create first verison

--- geographicalgranularity_widget/geographicalgranularity.js
+++ geographicalgranularity_widget/geographicalgranularity.js
@@ -1,19 +1,38 @@
-Videbligo.directive('search', ['MetadataService', function(MetadataService) {
+Videbligo.directive('geographicalGranularity', ['MetadataService', function(MetadataService) {
 
     return {
         restrict: 'AE',
         templateUrl: 'geographicalgranularity_widget/geographicalgranularity.html',
-        scope: {},
+        scope: {
+            orientation : '@'
+        },
         link: function(scope, element, attrs) {
             scope.data = null;
             scope.geographicalDimension = null;
+            scope.geographicalGranularityGroups = null;
+            scope.geographicalGranularity = geographicalGranularity;
+            if(attrs.orientation == undefined) {
+                scope.orientation = 'vertical';
+            }
 
             scope.init = function(){
                 scope.data = MetadataService.getData();
-                scope.geographicalDimension = scope.data.dimension(function(d){return d;});
+                scope.geographicalDimension = scope.data.dimension(function(d){return d.extras.geographical_granularity;});
+                scope.geographicalGranularityGroups = scope.geographicalDimension.group().all();
+                scope.mapGranularities();
             }
 
+            scope.mapGranularities = function(){
+                for(var i in scope.geographicalGranularityGroups) {
+                    if(scope.geographicalGranularityGroups[i].key != '') {
+                        scope.geographicalGranularity[scope.geographicalGranularityGroups[i].key].elements = scope.geographicalGranularityGroups[i].value;
+                    } else {
+                        scope.geographicalGranularity.Others.elements = scope.geographicalGranularityGroups[i].value;
+                    }
+                }
+            }
             scope.$on('filterChanged', function() {
+                scope.mapGranularities();
             });
 
             MetadataService.registerWidget(scope.init);
