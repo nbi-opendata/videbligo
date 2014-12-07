5e32b11b9c5133296502be94adcd63313d58c723
kainkordian <kainkordian@gmail.com>
2014-12-08 00:17:28 +0100

    timeGranularity_widget aktualisiert und auf den Stand des geographicalGranularity-widgets gebracht. Allerdings ohne Ausgabe...
    In index.html <time-granularity> Tag hinzugefügt.

--- timegranularity_widget/timegranularity.js
+++ timegranularity_widget/timegranularity.js
@@ -3,17 +3,37 @@ Videbligo.directive('timeGranularity', ['MetadataService', function(MetadataServ
     return {
         restrict: 'AE',
         templateUrl: 'timegranularity_widget/timegranularity.html',
-        scope: {},
+        scope: {
+            orientation : '@'
+        },
         link: function(scope, element, attrs) {
             scope.data = null;
-            scope.geographicalDimension = null;
+            scope.dimTimeGranularity = null;
+            scope.groupTimeGranularity = null;
+            //scope.timeGranularity = {};
+            if (attrs.orientation == undefined) {
+                scope.orientation = 'vertical';
+            }
 
             scope.init = function(){
                 scope.data = MetadataService.getData();
-                scope.geographicalDimension = scope.data.dimension(function(d){return d;});
+                scope.dimTimeGranularity = scope.data.dimension(function(d){return d.extras.temporal_granularity;});
+                scope.groupTimeGranularity = scope.dimTimeGranularity.group().all();
+                scope.mapGranularities();
+            }
+
+            scope.mapGranularities = function(){
+                for(var i in scope.groupTimeGranularity) {
+                    if(scope.groupTimeGranularity[i].key != '') {
+                        scope.timeGranularity[scope.groupTimeGranularity[i].key].elements = scope.groupTimeGranularity[i].value;
+                    } else {
+                        scope.timeGranularity.Others.elements = scope.groupTimeGranularity[i].value;
+                    }
+                }
             }
 
             scope.$on('filterChanged', function() {
+                scope.mapGranularities();
             });
 
             MetadataService.registerWidget(scope.init);
