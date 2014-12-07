db7b2b65709e7c783cb66b40a631487f7f3bbd7c
Martin Sonnenholzer <martin.sonnenholzer@googlemail.com>
2014-12-07 22:53:09 +0100

    renamed Widget

--- timegranularity_widget/timegranularity.js
+++ timegranularity_widget/timegranularity.js
@@ -1,4 +1,4 @@
-Videbligo.directive('search', ['MetadataService', function(MetadataService) {
+Videbligo.directive('timeGranularity', ['MetadataService', function(MetadataService) {
 
     return {
         restrict: 'AE',
