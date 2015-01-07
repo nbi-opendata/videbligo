8c6b3e3552ba12ddb9780889547f3928f2c65a0a
NormanRehbein <rehbein.norman@gmail.com>
2015-01-07 18:06:56 +0100

    commented some function, changed color variables to global, changed Name from 'map' to 'karte'

--- map_widget/map.js
+++ map_widget/map.js
@@ -1,3 +1,9 @@
+// Hier kann man zentral die Farben der Karte ändern
+
+colorUnclicked = '#73CAC6';
+colorClicked = '#00D38A';
+colorHover = '#FF8482';
+
 Videbligo.directive('map', ['MetadataService', function(MetadataService) {
 
     return {
@@ -7,9 +13,9 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
         link: function(scope, element, attrs) {
             scope.districts = [];
             scope.dataTemp = {};
-            scope.regionsAll = ["Pankow", "Berlin-Mitte", "Lichtenberg", "Marzahn-Hellersdorf", "Reinickendorf", "Spandau",
-                "Treptow-Köpenick", "Neu-Köln", "Tempelhof-Schöneberg", "Steglitz-Zehlendorf", "Friedrichshain-Kreuzberg",
-                "Charlottenburg-Wilmersdorf", "Berlin"
+            scope.regionsAll = ['Pankow', 'Berlin-Mitte', 'Lichtenberg', 'Marzahn-Hellersdorf', 'Reinickendorf', 'Spandau',
+                'Treptow-Köpenick', 'Neukölln', 'Tempelhof-Schöneberg', 'Steglitz-Zehlendorf', 'Friedrichshain-Kreuzberg',
+                'Charlottenburg-Wilmersdorf', 'Berlin'
             ];
 
             // alphabetisch sortieren
@@ -19,23 +25,19 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
                 scope.RegData = MetadataService.getData();
 
                 // Dimension erstellen, und diese dann gruppieren
-                scope.dimRegion = scope.RegData.dimension(function(d){return d.extras["geographical_coverage"];});
+                scope.dimRegion = scope.RegData.dimension(function(d){return d.extras['geographical_coverage'];});
                 scope.groupRegion = scope.dimRegion.groupAll().reduce(scope.reduceAdd, scope.reduceRemove, scope.reduceInitial);
-                scope.selected_map = new StringSet();
 
+                // Daten von den Regionen in ein Array stecken
                 scope.regionsAll.forEach(function(region){
                     var value = 0;
                     if(scope.groupRegion.value()[region] != undefined) {
                         value = scope.groupRegion.value()[region];
                     }
                     scope.districts.push({key:region , value: value, checked: true });
-                    scope.dataTemp[region] = ({key:region, value: value, hover:false, clicked:false});
+                    scope.dataTemp[region] = ({key:region, value: value});
                 })
-                scope.dummyData = scope.dataTemp;
-                d3.select("#mapInfoBox")
-                    .html('<div id="mapChart" ng-click="resetSelection()" style="position:absolute; right:30%;  visibility:hidden; border:2px solid cornflowerblue; background-color:yellow;">' +
-                    'WRECK IT RALPH!!!  ' + scope.dataTemp["Lichtenberg"].value +
-                    '</div>');
+                scope.regionData = scope.dataTemp;
             };
 
             MetadataService.registerWidget(scope.init);
@@ -48,17 +50,15 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
                     }
                     district.value = value;
                     // test
-                    scope.dummyData[district.key].value = value;
+                    scope.regionData[district.key].value = value;
                 });
             });
 
-
             scope.regionChoice = function(key) {
-
                 var checkedRegion = [];
-                for (var obj in scope.dummyData)
-                    if (scope.dummyData[obj].clicked){
-                        checkedRegion.push(scope.dummyData[obj].key);
+                for (var obj in scope.regionData)
+                    if (scope.regionData[obj].clicked){
+                        checkedRegion.push(scope.regionData[obj].key);
                     }
 
                 var filterFunction = function(d){
@@ -75,7 +75,7 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
             };
 
             scope.reduceAdd = function(p, v) {
-                var val = v.extras["geographical_coverage"];
+                var val = v.extras['geographical_coverage'];
                 if(val === undefined || val === null)
                     return p;
                 p[val] = (p[val]|| 0) + 1;
@@ -83,7 +83,7 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
             }
 
             scope.reduceRemove= function (p, v) {
-                var val = v.extras["geographical_coverage"];
+                var val = v.extras['geographical_coverage'];
                 if(val === undefined || val === null)
                     return p;
                 p[val] = (p[val]|| 0) - 1;
@@ -93,7 +93,6 @@ Videbligo.directive('map', ['MetadataService', function(MetadataService) {
             scope.reduceInitial = function() {
                 return {};
             }
-
         }
     };
 }]);
@@ -107,10 +106,10 @@ Videbligo.directive('svgMap', ['$compile', function ($compile) {
             var regions = element[0].querySelectorAll('.bezirk');
             angular.forEach(regions, function (path, key) {
                 var regionElement = angular.element(path);
-                regionElement.attr("region", "");
-                regionElement.attr("dummy-data", "dummyData");
-                regionElement.attr("hover-region", "hoverRegion");
-                regionElement.attr("regions-all","regionsAll")
+                regionElement.attr('region', '');
+                regionElement.attr('region-data', 'regionData');
+                regionElement.attr('hover-region', 'hoverRegion');
+                regionElement.attr('regions-all','regionsAll')
                 $compile(regionElement)(scope);
             })
         }
@@ -122,75 +121,70 @@ Videbligo.directive('region', ['$compile', function ($compile) {
     return {
         restrict: 'AE',
         scope: {
-            dummyData: "=",
+            regionData: "=",
             hoverRegion: "=",
             regionsAll: "="
         },
         link: function (scope, element, attrs) {
-            // scope.selected_map = new StringSet();
             scope.elementId = element.attr("id");
-            scope.colorUnclicked = "#FF0000";
-            scope.colorClicked = "#00FF00";
-            scope.colorHover = "#0000FF";
+
+            // Hier kann man die Farbe der Karte ändern
+            /*
+             scope.colorUnclicked = '#FF0000';
+             scope.colorClicked = '#00FF00';
+             scope.colorHover = '#0000FF';
+             */
 
             scope.regionClick = function () {
-                scope.dummyData[scope.elementId].clicked = !scope.dummyData[scope.elementId].clicked;
-                if(element[0].getAttribute("fill") !=scope.colorClicked){
-                    element[0].setAttribute("fill", scope.colorClicked);
+                if(element[0].getAttribute("fill") !=colorClicked){
+                    element[0].setAttribute("fill", colorClicked);
                 }
                 else{
-                    element[0].setAttribute("fill", scope.colorHover);
+                    element[0].setAttribute("fill", colorHover);
                 }
             };
 
             scope.regionMouseOver = function () {
                 scope.hoverRegion = scope.elementId;
                 element[0].parentNode.appendChild(element[0]);
-                scope.dummyData[scope.elementId].hover = true;
                 element[0].style.strokeWidth = 2.5;
-                element[0].style.stroke = "#000";
+                element[0].style.stroke = '#000';
 
-                if(element[0].getAttribute("fill") !=scope.colorClicked){
-                    element[0].setAttribute("fill", scope.colorHover);
+                if(element[0].getAttribute("fill") !=colorClicked){
+                    element[0].setAttribute("fill", colorHover);
                 }
-               d3.select("#mapChart").text(scope.elementId + "(" + scope.dummyData[scope.elementId].value + ")");
-               $("#mapChart").css("visibility", "visible");
+                d3.select('#mapChart').text(scope.elementId + "(" + scope.regionData[scope.elementId].value + ")");
+                $('#mapChart').css('visibility', 'visible');
             };
 
             scope.regionMouseLeave = function(){
-                scope.dummyData[scope.elementId].hover = false;
                 element[0].style.strokeWidth = 2;
-                element[0].style.stroke = "#fff";
-                if(element[0].getAttribute("fill") !=scope.colorClicked){
-                    element[0].setAttribute("fill", scope.colorUnclicked);
+                element[0].style.stroke = '#fff';
+                if(element[0].getAttribute('fill') !=colorClicked){
+                    element[0].setAttribute('fill', colorUnclicked);
                 }
-                $("#mapChart").css("visibility", "hidden");
+                $('#mapChart').css('visibility', 'hidden');
             }
 
-            /*
-            scope.regionClass = function(){
-                return {active:hoverRegion== elementId};
-            }
-            */
-            element.attr("ng-click", "regionClick()");
-            element.attr("ng-attr-fill", "{{dummyData[elementId] | map_color}}");
-            //element.attr("ng-attr-fill", "{{dummyData[elementId].value | map_color}}");
-            element.attr("ng-mouseover", "regionMouseOver()");
-            element.attr("ng-mouseleave", "regionMouseLeave()");
-            element.attr("ng-class", "{active:hoverRegion == elementId}");
-
-            element.removeAttr("region");
+            element.attr('ng-click', 'regionClick()');
+            element.attr('ng-attr-fill', '{{regionData[elementId] | map_color}}');
+            element.attr('ng-mouseover', 'regionMouseOver()');
+            element.attr('ng-mouseleave', 'regionMouseLeave()');
+            element.attr('ng-class', '{active:hoverRegion == elementId}');
+            element.removeAttr('region');
             $compile(element)(scope);
         }
     }
 }]);
 
- Videbligo.filter('map_color', [function () {
-     return function (input) {
-         var color = "#FF0000";
-         return color;
-     }
- }]);
+Videbligo.filter('map_color', [function () {
+    return function (input) {
+
+        // Initialisierungsfarbe der Regionen. Sollte äquivalent zu colorUnclicked sein
+        // var color = '#FF0000';
+        return colorUnclicked;
+    }
+}]);
 
 Videbligo.directive('tooltip', function () {
     return {
@@ -199,23 +193,7 @@ Videbligo.directive('tooltip', function () {
         {
             $(element)
                 .attr('title',scope.$eval(attrs.tooltip))
-                //.attr('title', element.attr("id"))
-                .tooltip({placement: "right"});
+                .tooltip({placement: 'right'});
         }
     }
 })
-// Basic color of map
-/*
-Videbligo.filter('map_color', [function () {
-    return function (input) {
-        if(input.hover){
-            return "#0000FF";
-        }
-        return "#5b95bc";
-
-    }
-}]);
-*/
-
-
-
