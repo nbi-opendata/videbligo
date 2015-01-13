414c824b350e15cee2a20f75ebfe62b74e7e0b0c
Wojciech Lukasiewicz <w.l@fu-berlin.de>
2015-01-13 16:57:53 +0100

    improved

--- date_widget/date.js
+++ date_widget/date.js
@@ -18,6 +18,7 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                 ADD: {},
                 REMOVE: {}
             };
+            scope.brushParams = {};
 
             scope.init = function(){
                 scope.data = MetadataService.getData();
@@ -32,9 +33,8 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     function (p,v){
                         var from = v.extras["temporal_coverage-from"];
                         var to = v.extras["temporal_coverage-to"];
-                        var fromDate = {};
-                        var toDate = to == undefined ? new Date() : parseDate(to);
                         if (from != undefined){
+                            var toDate = to == undefined ? new Date() : parseDate(to);
                             for (var i = parseDate(from).getFullYear(); i <= toDate.getFullYear(); i++){
                                 if (i in p.years){
                                     p.years[i]++;
@@ -49,9 +49,8 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     function (p,v) {
                         var from = v.extras["temporal_coverage-from"];
                         var to = v.extras["temporal_coverage-to"];
-                        var fromDate = {};
-                        var toDate = to == undefined ? new Date() : parseDate(to);
                         if (from != undefined){
+                            var toDate = to == undefined ? new Date() : parseDate(to);
                             for (var i = parseDate(from).getFullYear(); i <= toDate.getFullYear(); i++){
                                 p.years[i]--;
                                 if (p.years[i] === 0){
@@ -80,26 +79,59 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     - kleinere Unten- und Linkswerte schneiden die Achsenbeschriftungen ab
                     - NUR mit der Breite und Hoehe darf fuer die Anpassung gespielt werden
                  */
-                scope.svgParams.margin = {top: 0, right: 1, bottom: 30, left: 27},
-                    scope.svgParams.width = 750,
-                    scope.svgParams.height = 200;
+                scope.svgParams.mainMargin = {top: 5, right: 1, bottom: 130, left: 27};
+                scope.svgParams.miniMargin = {top: 235, right: 1, bottom: 30, left: 27};
+                scope.svgParams.width = 750;
+                scope.svgParams.mainHeight = 300 - scope.svgParams.mainMargin.top - scope.svgParams.mainMargin.bottom;
+                scope.svgParams.miniHeight = 300 - scope.svgParams.miniMargin.top - scope.svgParams.miniMargin.bottom;
 
-                scope.svgParams.x = d3.scale.ordinal().rangeRoundBands([0, scope.svgParams.width], .1);
-                scope.svgParams.y = d3.scale.linear().range([scope.svgParams.height, 0]);
+                scope.svgParams.zoom = d3.scale.linear()
+                    .range([0, scope.svgParams.width])
+                    .domain([0, scope.svgParams.width]);
 
-                scope.svgParams.x.domain(scope.svgParams.initialYears);
+                scope.svgParams.x = d3.scale.ordinal()
+                    .domain(scope.svgParams.initialYears)
+                    .rangeRoundBands([0, scope.svgParams.width], 0.1);
+                scope.svgParams.xMini = d3.scale.ordinal()
+                    .domain(scope.svgParams.initialYears)
+                    .rangeRoundBands([0, scope.svgParams.width], 0.1);
+
+                scope.svgParams.y = d3.scale.linear().range([scope.svgParams.mainHeight, 0]);
+                scope.svgParams.yMini = d3.scale.linear().range([scope.svgParams.miniHeight, 0]);
+
+                //scope.svgParams.x.domain(scope.svgParams.initialYears);
+                //scope.svgParams.xMini.domain(scope.svgParams.initialYears);
                 var currentValues = [];
                 for(var key in scope.svgParams.currentMappings) {
                     currentValues.push(scope.svgParams.currentMappings[key].value);
                 }
                 scope.svgParams.y.domain([0, d3.max(currentValues)]);
+                scope.svgParams.yMini.domain([0, d3.max(currentValues)]);
+
+
+
+                scope.brushParams.start = scope.svgParams.xMini(scope.svgParams.initialYears[0]);
+                scope.brushParams.end = scope.svgParams.xMini(scope.svgParams.initialYears[scope.svgParams.initialYears.length - 1]);
+                scope.brushParams.diff = //scope.svgParams.xMini.rangeBand() +
+                    scope.brushParams.end - scope.brushParams.start;
+                scope.brushParams.step = scope.brushParams.diff / scope.svgParams.initialYears.length;
+                scope.brushParams.brushedDomain = [];
 
                 scope.svgParams.xAxis = d3.svg.axis()
                     .scale(scope.svgParams.x)
                     .orient("bottom");
 
+                scope.svgParams.xAxisMini = d3.svg.axis()
+                    .scale(scope.svgParams.xMini)
+                    .orient("bottom");
+
                 scope.svgParams.yAxis = d3.svg.axis()
                     .scale(scope.svgParams.y)
+                    .orient("left")
+                    .tickFormat(d3.format("d"));
+
+                scope.svgParams.yAxisMini = d3.svg.axis()
+                    .scale(scope.svgParams.yMini)
                     .orient("left")
                     .tickFormat(d3.format("d"));
 
@@ -109,15 +141,20 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     .attr("style", "cursor: pointer; display: block; visibility: hidden;")
                     .text("reset");
 
-                scope.svg = d3.select("#time-chart").append("svg")
-                    .attr("width", scope.svgParams.width + scope.svgParams.margin.left + scope.svgParams.margin.right)
-                    .attr("height", scope.svgParams.height + scope.svgParams.margin.top + scope.svgParams.margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + scope.svgParams.margin.left + "," + scope.svgParams.margin.top + ")");
+                var svg = d3.select("#time-chart").append("svg")
+                    .attr("width", scope.svgParams.width + scope.svgParams.mainMargin.left + scope.svgParams.mainMargin.right)
+                    .attr("height", scope.svgParams.mainHeight + scope.svgParams.mainMargin.top + scope.svgParams.mainMargin.bottom);
 
-                scope.svg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + scope.svgParams.height + ")")
+                scope.main = svg.append("g")
+                    .attr("transform", "translate(" + scope.svgParams.mainMargin.left + "," + scope.svgParams.mainMargin.top + ")");
+
+                scope.mini = svg.append("g")
+                    .attr("transform", "translate(" + scope.svgParams.miniMargin.left + "," + scope.svgParams.miniMargin.top + ")");
+
+                scope.main.append("g")
+                    .attr("id", "xAxis")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + scope.svgParams.mainHeight + ")")
                     .call(scope.svgParams.xAxis)
                     .selectAll("text")
                     .style("text-anchor", "end")
@@ -125,8 +162,9 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     .attr("dy", "-.45em")
                     .attr("transform", function(d){return "rotate(-80)"});
 
-                scope.svg.append("g")
-                    .attr("class", "y axis")
+                scope.main.append("g")
+                    .attr("class", "axis")
+                    .attr("id", "yAxis")
                     .call(scope.svgParams.yAxis)
                     .append("text")
                     .attr("transform", "rotate(-90)")
@@ -135,7 +173,18 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     .style("text-anchor", "end")
                     .text("Anzahl der Datensätze");
 
-                var onData = scope.svg.selectAll(".bar")
+                scope.mini.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + scope.svgParams.miniHeight + ")")
+                    .call(scope.svgParams.xAxisMini)
+                    .selectAll("text")
+                    .style("text-anchor", "end")
+                    .attr("dx", "-.8em")
+                    .attr("dy", "-.45em")
+                    .attr("transform", function(d){return "rotate(-80)"});
+
+
+                var onData = scope.main.selectAll(".bar")
                     .data(scope.svgParams.currentMappings);
 
                 onData
@@ -144,7 +193,7 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     .attr("x", function(d) { return scope.svgParams.x(d.year); })
                     .attr("width", scope.svgParams.x.rangeBand())
                     .attr("y", "0")
-                    .attr("height", function(d) { return scope.svgParams.height; })
+                    .attr("height", function(d) { return scope.svgParams.mainHeight; })
                     .attr("ng-mousedown", function(d){ return "handleMouseDown('"+ d.year+"')";})
                     .attr("ng-class", function(d){ return "{'barbg' : true, 'active': selectedYears.contains("+ d.year+")}";})
                     .attr("ng-mouseover", function(d){ return "handleHover($event, '"+ d.year+"', '"+d.value+"')";})
@@ -157,11 +206,33 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     .attr("id", function(d) { return "time-chart-bar-"+d.year; })
                     .attr("width", scope.svgParams.x.rangeBand())
                     .attr("y", function(d) { return scope.svgParams.y(d.value); })
-                    .attr("height", function(d) { return scope.svgParams.height - scope.svgParams.y(d.value); })
+                    .attr("height", function(d) { return scope.svgParams.mainHeight - scope.svgParams.y(d.value); })
                     .attr("ng-mousedown", function(d){ return "handleMouseDown('"+ d.year+"')";})
                     .attr("ng-class", function(d){ return "{'bar': true, 'active': selectedYears.contains("+ d.year+")}";})
                     .attr("ng-mouseover", function(d){ return "handleHover($event, '"+ d.year+"', '"+d.value+"')";})
                     .attr("ng-mouseleave", function(d){ return "resetHovers()";});
+
+
+                scope.mini.selectAll(".bar")
+                    .data(scope.svgParams.currentMappings)
+                    .enter()
+                    .append("rect")
+                    .attr("id", function(d) { return "time-chart-bar-mini-"+d.year; })
+                    .attr("x", function(d) { return scope.svgParams.xMini(d.year); })
+                    .attr("width", scope.svgParams.xMini.rangeBand())
+                    .attr("y", function(d) { return scope.svgParams.yMini(d.value); })
+                    .attr("height", function(d) { return scope.svgParams.miniHeight - scope.svgParams.yMini(d.value); });
+
+                scope.brush = d3.svg.brush()
+                    .x(scope.svgParams.xMini)
+                    .on("brush", scope.brushed);
+
+                scope.mini.append("g")
+                    .attr("class", "x brush")
+                    .call(scope.brush)
+                    .selectAll("rect")
+                    .attr("y", -10)
+                    .attr("height", scope.svgParams.miniHeight + 15);
 
                 $compile(angular.element('#time-chart'))(scope);
             };
@@ -169,29 +240,42 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
             scope.$on('filterChanged', function() {
                 scope.updateCurrentMappings();
 
-                scope.svgParams.y.domain([0, d3.max(scope.svgParams.currentMappings, function (d) { return d.value; })]);
-                scope.svgParams.yAxis = d3.svg.axis()
-                    .scale(scope.svgParams.y)
-                    .tickFormat(d3.format("d"))
-                    .orient("left");
+                var usedMappings = {};
+                if (!scope.brush.empty()){
+                    usedMappings = scope.brushParams.brushedMappings;
+                }
+                else {
+                    usedMappings = scope.svgParams.currentMappings;
 
-                scope.svg.select("g .y.axis").call(scope.svgParams.yAxis);
+                }
+                scope.svgParams.y.domain([0, d3.max(usedMappings, function (d) { return d.value; })]);
+                scope.svgParams.yMini.domain([0, d3.max(usedMappings, function (d) { return d.value; })]);
+                scope.svgParams.yAxis.scale(scope.svgParams.y);
+                scope.svgParams.yAxisMini.scale(scope.svgParams.yMini);
 
-                var minMappings = {};
+                scope.main.select("#yAxis").call(scope.svgParams.yAxis);
+
+                var simplifiedGlobalMappings = {};
                 for (var key in scope.svgParams.currentMappings){
-                    minMappings[scope.svgParams.currentMappings[key].year] = scope.svgParams.currentMappings[key].value;
+                    simplifiedGlobalMappings[scope.svgParams.currentMappings[key].year] = scope.svgParams.currentMappings[key].value;
                 }
 
                 for (var key in scope.svgParams.initialYears){
                     var year = scope.svgParams.initialYears[key];
-                    var chartBar = angular.element("#time-chart-bar-"+year);
-                    if (minMappings[year]){
-                        chartBar
-                            .attr("y", scope.svgParams.y(minMappings[year]))
-                            .attr("height", function(d) { return scope.svgParams.height - scope.svgParams.y(minMappings[year]); });
+                    var chartBarMain = angular.element("#time-chart-bar-"+year);
+                    var chartBarMini = angular.element("#time-chart-bar-mini-"+year);
+
+                    if (simplifiedGlobalMappings[year]){
+                        chartBarMain
+                            .attr("y", scope.svgParams.y(simplifiedGlobalMappings[year]))
+                            .attr("height", function(d) { return scope.svgParams.mainHeight - scope.svgParams.y(simplifiedGlobalMappings[year]); });
+                        chartBarMini
+                            .attr("y", scope.svgParams.yMini(simplifiedGlobalMappings[year]))
+                            .attr("height", function(d) { return scope.svgParams.miniHeight - scope.svgParams.yMini(simplifiedGlobalMappings[year]); });
                     }
                     else {
-                        chartBar.attr("height", "0");
+                        chartBarMain.attr("height", "0");
+                        chartBarMini.attr("height", "0");
                     }
                 }
             });
@@ -315,6 +399,100 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
             scope.resetHovers = function () {
                 scope.hover.year = "";
                 scope.hover.value = "";
+            };
+
+            scope.brushed = function () {
+                var originalRange = scope.svgParams.zoom.range();
+                scope.svgParams.zoom.domain(scope.brush.empty() ?
+                    originalRange:
+                    scope.brush.extent());
+
+                var brushStart = scope.svgParams.zoom.domain()[0];
+                var brushEnd = scope.svgParams.zoom.domain()[1];
+
+                var startYear = scope.brushPositionToYear(brushStart);
+                var endYear = scope.brushPositionToYear(brushEnd);
+
+                scope.brushParams.brushedMappings = [];
+                var valuesWithinBrushedArea = scope.svgParams.currentMappings.map(function(d){
+                    if ((parseInt(d.year) >= startYear && parseInt(d.year) <= endYear)){
+                        scope.brushParams.brushedMappings.push(d);
+                        return d.value;
+                    }
+                    return 0;
+                });
+
+                scope.svgParams.y.domain([0, d3.max(valuesWithinBrushedArea)]);
+                scope.svgParams.yMini.domain([0, d3.max(valuesWithinBrushedArea)]);
+
+                scope.main.select("#yAxis").call(scope.svgParams.yAxis);
+
+                scope.brushParams.brushedDomain = [];
+                for (var i = scope.svgParams.initialYears.indexOf(startYear);
+                     i <= scope.svgParams.initialYears.indexOf(endYear); i++) {
+                    scope.brushParams.brushedDomain.push(scope.svgParams.initialYears[i]+"");
+                }
+                scope.svgParams.x.domain(scope.brushParams.brushedDomain);
+
+                scope.main.select("#xAxis")
+                    .call(scope.svgParams.xAxis)
+                    .selectAll("text")
+                    .style("text-anchor", "end")
+                    .attr("dx", "-.8em")
+                    .attr("dy", "-.45em")
+                    .attr("transform", function(d){return "rotate(-80)"});
+
+                scope.main
+                    .selectAll("rect")
+                    .attr("x", function (d) {
+                        var xPos = scope.svgParams.x(d.year);
+                        var tr = xPos;
+                        if (xPos + scope.svgParams.x.rangeBand() < scope.brushParams.start){
+                            xPos = -1000;
+                        }
+                        else if (xPos < scope.brushParams.start){
+                            xPos = scope.brushParams.start;
+                        }
+                        else
+                        if (xPos == undefined){
+                            return -1000;
+                        }
+                        return xPos;
+                    })
+                    .attr("width", function(d) {
+                        var xPos = scope.svgParams.x(d.year);
+                        if (xPos + scope.svgParams.x.rangeBand() < scope.brushParams.start){
+                            return 0;
+                        }
+                        else if (xPos < scope.brushParams.start){
+                            return xPos + scope.svgParams.x.rangeBand() - scope.brushParams.start;
+                        }
+                        return scope.svgParams.x.rangeBand();
+                    });
+
+                scope.main.selectAll(".bar")
+                    .attr("y", function(d) { return scope.svgParams.y(d.value); })
+                    .attr("height", function(d) { return scope.svgParams.mainHeight - scope.svgParams.y(d.value); });
+
+                scope.main.selectAll(".barbg")
+                    .attr("y", "0")
+                    .attr("height", function(d) { return scope.svgParams.mainHeight; });
+
+            };
+
+            scope.brushPositionToYear = function(brushPosition){
+                var distanceFromStart = brushPosition - scope.brushParams.start;
+                if (distanceFromStart < 0){
+                    distanceFromStart = 0;
+                }
+
+                var offset = Math.floor(distanceFromStart / scope.brushParams.step);
+
+                if (offset >= scope.svgParams.initialYears.length){
+                    offset = scope.svgParams.initialYears.length - 1;
+                }
+
+                return scope.svgParams.initialYears[offset];
             };
 
             MetadataService.registerWidget(scope.init);
