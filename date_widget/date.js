af7e1b95825ac37ff20f4de4af2022f9afb3e52b
ktugan <kadir.tugan@gmail.com>
2015-01-14 18:06:07 +0100

    working date widget with dc js

--- date_widget/date.js
+++ date_widget/date.js
@@ -12,32 +12,6 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
             scope.groupDate = {};
             scope.dimDate = {};
 
-            scope.extractGroupData = function(d){
-                var groupValues = [];
-                d = d[0].value;
-                for(var key in d){
-                    var v = {
-                        'key': parseInt(key),
-                        'value': d[key]
-                    }
-                    groupValues.push(v)
-                }
-                return groupValues;
-            }
-
-            scope.groupWrapper = {
-                'all': function(){return scope.extractGroupData(scope.groupDate.all());},
-                'top': function(n){return scope.extractGroupData(scope.groupDate.all());},
-                'size': function(){return scope.extractGroupData(scope.groupDate.all()).length;}
-            }
-
-            scope.accessLeft = function(d) {
-                return d.extras["temporal_coverage-from"];
-            }
-
-            scope.accessRight = function(d){
-                return d.extras["temporal_coverage-to"];
-            }
             scope.getD3TimeRange = function(data){
                 var formatter = d3.time.year;
                 var dateFrom = parseDate(data.extras["temporal_coverage-from"]);
@@ -59,26 +33,22 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                 var data = MetadataService.getData();
 
                 scope.dimDate = data.dimension(function(d){
-                    return scope.getD3TimeRange(d);
-                });
-
-                scope.dimDateFrom = data.dimension(function(d){
-                    return parseDate(scope.accessLeft(d));
-                });
-
-                scope.dimDateTo = data.dimension(function(d){
-                    return parseDate(scope.accessRight(d));
+                    return scope.getD3TimeRange(d).map(function (date) {
+                        return date.getFullYear();
+                    });
                 });
 
                 scope.groupDate = scope.dimDate.groupAll().reduce(
                     function (p,v){
                         scope.getD3TimeRange(v).forEach (function(val, idx) {
+                            val = val.getFullYear();
                             p[val] = (p[val] || 0) + 1; //increment counts
                         });
                         return p;
                     },
                     function (p,v) {
                         scope.getD3TimeRange(v).forEach (function(val, idx) {
+                            val = val.getFullYear();
                             p[val] = (p[val] || 0) - 1; //increment counts
                         });
                         return p;
@@ -88,8 +58,7 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     }
                 );
 
-
-
+                //adding functions to simulate the group of crossfilter
                 scope.groupDate.all = function() {
                     var newObject = [];
                     var self = this.value();
@@ -102,18 +71,12 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                         }
                     }
 
-                    newObject.sort(function(a,b){
-                        return new Date(a.key) - new Date(b.key);
-                    });
-
                     return newObject;
                 };
 
                 scope.groupDate.size = function(){return this.all().length;};
-                scope.groupDate.top = function(n){console.log('top'); return this.all()};
 
                 scope.initSvg();
-                //var z = x.filter(function(d){return d.map(function(d){return d.getFullYear();}).indexOf(parseDate('2014-01-02').getFullYear()) > -1;})
             }
 
 
@@ -122,64 +85,23 @@ Videbligo.directive('date', ['MetadataService', '$compile', function(MetadataSer
                     MetadataService.triggerUpdate(this);
                 }, 250);
 
-                var dates = Object.keys(scope.groupDate).map(function(d){
-                    return new Date(d);
-                }).sort(function(a,b){
-                    return new Date(a) - new Date(b);
-                });
-
-
-
-                var first = dates[0];
-                var length = dates.length;
-                var last = dates[length-1];
-
-                //scope.chart = dc.barChart('#time-chart');
-                //var chart = scope.chart;
-                //chart.width(600)
-                //    .height(200)
-                //    .margins({top: 0, right: 50, bottom: 20, left: 40})
-                //    .dimension(scope.dimDate)
-                //    .group(scope.groupDate)
-                //
-                //    .xUnits(d3.time.years)
-                //    .gap(1)
-                //    //.round(dc.round.floor)
-                //    .transitionDuration(1000)
-                //    .x(d3.time.scale().domain([first, last]));
-                ////chart.xAxis().tickFormat(function (v) {return v;});
-
+                var first = parseInt(scope.groupDate.all()[0].key);
+                var length = scope.groupDate.size();
+                var last = parseInt(scope.groupDate.all()[length -1].key);
 
                 var chart = dc.barChart('#time-chart');
                 scope.chart = chart;
                 chart
                     .width(1000)
                     .height(200)
-                    //.renderLabel(true)
                     .dimension(scope.dimDate)
                     .group(scope.groupDate)
-                    .x(d3.time.scale().domain([first, last]))
-                    .xAxis().ticks(3);
-                chart.round(d3.time.year);
-
-
-                chart.on("filtered", function(chart, filter){
-                    scope.debounceTriggerUpdate(chart, filter);
-                });
-
-                chart.filterHandler(function(dimension, filter){
-                    console.log(filter);
-                    //dimension.filter(function(d){
-                    //    return d.map(function(d){return d.getFullYear();})
-                    //            .indexOf(filter.getFullYear()) > -1;
-                    //});
-                    return filter;
-                });
+                    .x(d3.scale.linear().domain([first, last]))
+                    //.elasticX(true)
+                    .elasticY(true);
 
                 chart.render();
             }
-
-
 
             scope.$on('filterChanged', function() {
                 scope.chart.redraw();
