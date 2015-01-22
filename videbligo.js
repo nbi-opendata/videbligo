3f5ea2499d59865340163531fcfd285d335754a5
Wojciech Lukasiewicz <w.l@fu-berlin.de>
2015-01-22 10:58:28 +0100

    added german locale variable

--- videbligo.js
+++ videbligo.js
@@ -138,6 +138,21 @@ function debounce(func, wait, immediate) {
     };
 }
 
+germanFormatter = d3.locale({
+    "decimal": ".",
+    "thousands": ",",
+    "grouping": [3],
+    "currency": ["$", ""],
+    "dateTime": "%a %b %e %X %Y",
+    "date": "%m/%d/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+    "shortDays": ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+    "months": ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    "shortMonths": ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
+});
+
 
 
 
