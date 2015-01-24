bfad42bcd347eb7ff7fb83c6bdffdcf4b123ea1c
lbenedix <lukas.benedix@fu-berlin.de>
2015-01-24 21:39:12 +0100

    refactoring of map widget

--- map_widget/map.js
+++ map_widget/map.js
@@ -2,66 +2,97 @@ onSvg = false;
 allBerlin = null;
 bezirke = null;
 
-
-Videbligo.directive('map', ['MetadataService', '$compile', function(MetadataService, $compile) {
+Videbligo.directive('map', ['MetadataService', '$compile', function (MetadataService, $compile) {
 
     return {
         restrict: 'AE',
         templateUrl: 'map_widget/map.html',
         scope: {},
-        link: function(scope, element, attrs) {
+        link: function (scope, element, attrs) {
             scope.districts = [];
             scope.dataTemp = {};
             scope.regionsAll = ['Pankow', 'Berlin-Mitte', 'Lichtenberg', 'Marzahn-Hellersdorf', 'Reinickendorf', 'Spandau',
                 'Treptow-Köpenick', 'Neukölln', 'Tempelhof-Schöneberg', 'Steglitz-Zehlendorf', 'Friedrichshain-Kreuzberg',
                 'Charlottenburg-Wilmersdorf', 'Berlin'
             ];
-            scope.showBerlinBorder = false;
-            scope.showMapChart = false;
+
+            scope.selected_districts = new StringSet();
+            scope.hovered_district = 'none';
 
             // alphabetisch sortieren
             scope.regionsAll.sort();
 
-            scope.init = function() {
+            scope.init = function () {
                 scope.RegData = MetadataService.getData();
 
                 // Dimension erstellen, und diese dann gruppieren
-                scope.dimRegion = scope.RegData.dimension(function(d){return d.extras['geographical_coverage'];});
+                scope.dimRegion = scope.RegData.dimension(function (d) {return d.extras['geographical_coverage'];});
                 scope.groupRegion = scope.dimRegion.groupAll().reduce(scope.reduceAdd, scope.reduceRemove, scope.reduceInitial);
 
                 // Daten(Name, Anzahl der Datensätze ) von den Regionen in eine Liste stecken
-                scope.regionsAll.forEach(function(region){
+                scope.regionsAll.forEach(function (region) {
                     var value = 0;
                     if(scope.groupRegion.value()[region] != undefined) {
                         value = scope.groupRegion.value()[region];
                     }
-                    scope.districts.push({key:region , value: value});
-                    scope.dataTemp[region] = ({key:region, value: value, clicked:false});
-                })
+                    scope.districts.push({key: region, value: value});
+                    scope.dataTemp[region] = ({key: region, value: value, clicked: false});
+                });
                 scope.regionData = scope.dataTemp;
 
-
-                //initializing map
-                var regions = element[0].querySelectorAll('.bezirk');
-                bezirke = regions;
-                angular.forEach(regions, function (path, key) {
+                /* add click and hover handler to each district */
+                angular.forEach(angular.element('.bezirk'), function (path, key) {
                     var regionElement = angular.element(path);
-                    regionElement.attr('region', '');
-                    regionElement.attr('region-data', 'regionData');
-                    regionElement.attr('regions-all','regionsAll')
-                    $compile(regionElement)(scope);
-                })
-                allBerlin = element[0].querySelector('.BerlinBorder');
-                allBerlin.attributes['ng-class'] = '{berlin_border_hover: showBerlinBorder}'
+                    var id = regionElement.attr('id');
+
+                    regionElement.attr('data-ng-click', 'clickDistrict("'+id+'")');
+                    regionElement.attr('data-ng-mouseenter', 'enterDistrict("'+id+'")');
+                    regionElement.attr('data-ng-mouseleave', 'leaveDistrict("'+id+'")');
+
+                    regionElement.attr('data-ng-class', '{\'selected\': selected_districts.contains("'+id+'")}');
+                });
+                $compile(angular.element('svg'))(scope);
             };
+
+
+            scope.clickDistrict = function(bezirk){
+                // toggle district in set of selected districts
+                if(scope.selected_districts.contains(bezirk)){
+                    scope.selected_districts.remove(bezirk);
+                }else{
+                    scope.selected_districts.add(bezirk);
+                }
+
+                var filterFunction = function (d) {
+                    return scope.selected_districts.contains(d);
+                };
+
+                if(scope.selected_districts.values().length == 0) {
+                    scope.dimRegion.filterAll();
+                }else{
+                    scope.dimRegion.filter(filterFunction);
+                }
+                MetadataService.triggerUpdate();
+
+                console.log(scope.regionData[bezirk]['value']);
+            };
+
+            scope.enterDistrict = function(bezirk){
+                scope.hovered_district = bezirk;
+            };
+
+            scope.leaveDistrict = function(bezirk){
+                scope.hovered_district = 'none';
+            };
+
 
             MetadataService.registerWidget(scope.init);
 
             // Hier werden die Werte geupdatet, wenn ein neuer Filter irgendwo gesetzt wird
-            scope.$on('filterChanged', function() {
-                scope.districts.forEach(function(district) {
+            scope.$on('filterChanged', function () {
+                scope.districts.forEach(function (district) {
                     var value = 0;
-                    if (scope.groupRegion.value()[district.key] != undefined){
+                    if(scope.groupRegion.value()[district.key] != undefined) {
                         value = scope.groupRegion.value()[district.key];
                     }
                     district.value = value;
@@ -69,135 +100,63 @@ Videbligo.directive('map', ['MetadataService', '$compile', function(MetadataServ
                 });
             });
 
-            // Wir setzen einen boolean, falls eine Region ausgewählt wird.
-            // Über diesen boolean, können über alle ausgewählten regionen iterieren und dann auf diesen Filtern
-            // Falls keine Region ausgewählt ist, werden die Filter zurückgesetzt
-            scope.regionChoice = function(key) {
-                var checkedRegion = [];
-                for (var obj in scope.regionData)
-                    if (scope.regionData[obj].clicked){
-                        checkedRegion.push(scope.regionData[obj].key);
-                    }
+            //// Wir setzen einen boolean, falls eine Region ausgewählt wird.
+            //// Über diesen boolean, können über alle ausgewählten regionen iterieren und dann auf diesen Filtern
+            //// Falls keine Region ausgewählt ist, werden die Filter zurückgesetzt
+            //scope.regionChoice = function (key) {
+            //    var checkedRegion = [];
+            //    for (var obj in scope.regionData) {
+            //        if(scope.regionData[obj].clicked) {
+            //            checkedRegion.push(scope.regionData[obj].key);
+            //        }
+            //    }
+            //
+            //    var filterFunction = function (d) {
+            //        return checkedRegion.indexOf(d) != -1;
+            //    };
+            //
+            //    if(checkedRegion.length != 0) {
+            //        scope.dimRegion.filter(filterFunction);
+            //    } else {
+            //        scope.dimRegion.filterAll();
+            //    }
+            //
+            //    MetadataService.triggerUpdate();
+            //};
 
-                var filterFunction = function(d){
-                    return checkedRegion.indexOf(d) != -1;
-                }
-
-                if(checkedRegion.length != 0){
-                    scope.dimRegion.filter(filterFunction);
-                } else {
-                    scope.dimRegion.filterAll();
-                }
-
-                MetadataService.triggerUpdate();
-            };
-
-            // filtert bei einem Klick >Berlin<,  wenn wir nicht auf der SVG-Karte sind
-            scope.berlinClicked = function(){
-                if(!onSvg){
-                    scope.regionData['Berlin'].clicked = !scope.regionData['Berlin'].clicked;
-                }
-            }
-
-            // zeigt hover an, und verstärkt den Rand, wenn wir nich über der SVG-Karte hovern
-            scope.berlinMouseOver = function(){
-                if(!onSvg) {
-                    scope.showBerlinBorder = true;
-                    d3.select('#mapChart').text("Berlin" + "(" + scope.regionData['Berlin'].value + ")");
-                    scope.showMapChart = true;
-                }
-            }
-
-            // blendet Hover wieder aus
-            scope.berlinMouseLeave = function(){
-                scope.showBerlinBorder = false;
-                d3.select('#mapChart').text("Berlin" + "(" + scope.regionData['Berlin'].value + ")");
-                scope.showMapChart = false;
-            }
 
 
             // reset-function
-            scope.reset = function(){
+            scope.reset = function () {
+                console.log('reset');
+                scope.selected_districts = new StringSet();
                 scope.$broadcast('reset');
                 scope.dimRegion.filterAll();
                 MetadataService.triggerUpdate();
-            }
-
+            };
 
             // Eigene Reduce-Funktionen zum individuellen Gruppieren
-            scope.reduceAdd = function(p, v) {
+            scope.reduceAdd = function (p, v) {
                 var val = v.extras['geographical_coverage'];
-                if(val === undefined || val === null)
+                if(val === undefined || val === null) {
                     return p;
-                p[val] = (p[val]|| 0) + 1;
+                }
+                p[val] = (p[val] || 0) + 1;
                 return p;
-            }
+            };
 
-            scope.reduceRemove= function (p, v) {
+            scope.reduceRemove = function (p, v) {
                 var val = v.extras['geographical_coverage'];
-                if(val === undefined || val === null)
+                if(val === undefined || val === null) {
                     return p;
-                p[val] = (p[val]|| 0) - 1;
+                }
+                p[val] = (p[val] || 0) - 1;
                 return p;
-            }
+            };
 
-            scope.reduceInitial = function() {
+            scope.reduceInitial = function () {
                 return {};
             }
         }
     };
 }]);
-
-// User Interface : Mause-Click, Mause-Over
-Videbligo.directive('region', ['$compile', function ($compile) {
-    return {
-        restrict: 'AE',
-        scope: {
-            regionData: "=",
-            regionsAll: "="
-        },
-        link: function (scope, element, attrs) {
-            scope.elementId = element.attr("id");
-            scope.hover = false;
-            scope.clicked = false;
-
-            scope.$on('reset', function() {
-                scope.hover = false;
-                scope.clicked = false;
-                scope.regionData[scope.elementId].clicked = scope.clicked;
-            });
-
-            // Falls eine Region angeklickt wird, setzen wir einen boolean um den Zustand zu speichern und ändern die Farbe
-            scope.regionClick = function () {
-                scope.clicked = !scope.clicked;
-                scope.regionData[scope.elementId].clicked = scope.clicked;
-            };
-
-            // Beim Hover / MouseOver verändern wir die Farbe der Region und zeigen das div pop-up an
-            scope.regionMouseOver = function () {
-                onSvg = true;
-                scope.hover = true;
-
-            };
-
-            // Beim MouseLeave setzen wir die richtige Farbe durch einen Vergleich
-            scope.regionMouseLeave = function(){
-                scope.hover = false;
-                onSvg = false;
-            }
-
-            // Hier werden Funktionen zugeordnet
-
-            element[0].style.strokeWidth = 2;
-            element[0].style.stroke = '#000';
-
-            element.attr('ng-click', 'regionClick()');
-            element.attr('ng-mouseenter', 'regionMouseOver()');
-            element.attr('ng-mouseleave', 'regionMouseLeave()');
-            element.attr('ng-class', '{hover: hover, selected: clicked, selected_hover: clicked && hover, standard: !hover && !clicked}');
-            element.removeAttr('region');
-            $compile(element)(scope);
-        }
-    }
-}]);
-
